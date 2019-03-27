214d6ae8b11a2f300864f660c902f5f0f325ef57
nsterdt <nico-sterdt@web.de>
2019-03-27 18:15:03 +0100

    inputs change css variables

--- JS03 - CSS Variables/script.js
+++ JS03 - CSS Variables/script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || "";
+    
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
